8bf900d70e93268212ee909839bbc2e36c2c368a
Martin Armbruster <armbrustermartin@googlemail.com>
2020-06-17 21:40:39 +0200

    Converted the test plugin to a Maven module.

--- Tests/org.emftext.language.java.test/src-input/resolving/importResolverTest/JavaUtilEnumerationImport.java
+++
@@ -1,8 +0,0 @@
-package resolving.importResolverTest;
-
-// it must be possible to resolve this Enumeration in the test
-import java.util.Formatter.BigDecimalLayoutForm;
-
-public class JavaUtilEnumerationImport {
-
-}
