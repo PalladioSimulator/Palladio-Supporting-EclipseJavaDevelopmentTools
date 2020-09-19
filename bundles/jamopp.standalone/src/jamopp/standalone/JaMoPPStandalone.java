2fc64a350a7b8c6de4f47563dc86b6193b5f75a7
Adrian <uqrfw@student.kit.edu>
2020-09-19 12:10:04 +0200

    Standalone version working properly

--- bundles/jamopp.standalone/src/jamopp/standalone/JaMoPPStandalone.java
+++ bundles/jamopp.standalone/src/jamopp/standalone/JaMoPPStandalone.java
@@ -1,22 +1,50 @@
 package jamopp.standalone;
 
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.EcoreUtil.EqualityHelper;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.emftext.language.java.containers.CompilationUnit;
+import org.emftext.language.java.containers.JavaRoot;
+import org.emftext.language.java.containers.Package;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-
-import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.EcoreUtil.EqualityHelper;
+import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 import org.emftext.language.java.containers.EmptyModel;
 import org.emftext.language.java.containers.JavaRoot;
+import org.emftext.language.java.containers.Package;
+import org.emftext.language.java.containers.CompilationUnit;
 import org.emftext.language.java.containers.ContainersFactory;
 
 import jamopp.parser.api.JaMoPPParserAPI;
@@ -37,30 +65,94 @@ import jamopp.resource.JavaResource2Factory;
 public class JaMoPPStandalone {
 	
 	private static final String INPUT = "/Users/adrianschneider/Desktop/DemoApplication.java";
-    private static final String OUTPUT = "./file.containers";
-	
+    
+    public static void main(String[] agrs) throws Exception {
+    	
+    	 JavaRoot root = ContainersFactory.eINSTANCE.createEmptyModel();  
+		 Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("java", new JavaResource2Factory());
+		 Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		 JaMoPPParserAPI parser = new JaMoPPJDTParser();
+		 ResourceSet rs = parser.parseFile(Path.of(INPUT)).getResourceSet();
+		 
+		
+		EcoreUtil.resolveAll(rs);
+		int emptyFileName = 0;
+		String outputFileName = "";
+		
+		
+		for (Resource javaResource : new ArrayList<Resource>(rs.getResources())) {
+			
+			
+			if (javaResource.getContents().isEmpty()) {
+				
+				System.out.println("WARNING: Emtpy Resource: " + javaResource.getURI());
+				continue;
+			}
+			
+			if(javaResource.getURI().scheme().equals("file")) {
+			
+			root = (JavaRoot) javaResource.getContents().get(0);
+			
+			if (root instanceof CompilationUnit) {
+				outputFileName = root.getNamespacesAsString().replace(".", File.separator) + File.separator;
+				CompilationUnit cu = (CompilationUnit) root;
+				if (cu.getClassifiers().size() > 0) {
+					outputFileName += cu.getClassifiers().get(0).getName();
+				} else {
+					outputFileName += emptyFileName++;
+				}
 
-	public static void main(String[] args) {
+			} else if (root instanceof Package) {
+				outputFileName = root.getNamespacesAsString().replace(".", File.separator) + File.separator + "package-info";
+				if (outputFileName.startsWith(File.separator)) {
+					outputFileName = outputFileName.substring(1);
+				}
+			} else if (root instanceof org.emftext.language.java.containers.Module) {
+				outputFileName = root.getNamespacesAsString().replace(".", File.separator) + File.separator + "module-info";
+			} 
+			File outputFile = new File("." + File.separator + "./standalone_output" + File.separator + outputFileName);
+			outputFile.getParentFile().mkdirs();
+			
+			URI xmiFileURI = URI.createFileURI(outputFile.getAbsolutePath()).appendFileExtension("xmi");	
+			Resource xmiResource = rs.createResource(xmiFileURI);
+			xmiResource.getContents().addAll(javaResource.getContents());
+		}
+		
+		}
+		
+		
+		for (Resource xmiResource : rs.getResources()) {
+			if (xmiResource instanceof XMIResource) {
+				try {
+					xmiResource.save(rs.getLoadOptions());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		
+		
+	}
+    
+    
+	
+    
+    
+  
+	public static void oldmain(String[] args) {
 		 JavaRoot root = ContainersFactory.eINSTANCE.createEmptyModel();  
 		 Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("java", new JavaResource2Factory());
 		 JaMoPPParserAPI parser = new JaMoPPJDTParser();
-		 ResourceSet rs = parser.parseFile(Path.of(INPUT)).getResourceSet().getResources();
-		 rs.createResource(URI.createFileURI(Path.of(INPUT)));
+		 ResourceSet rs = parser.parseFile(Path.of(INPUT)).getResourceSet();
+		 //rs.createResource(URI.createFileURI(Path.of(INPUT)));
 	     Resource gastResource = createAndAddResource("C:/file.gast", new String[] {"xml","gast"}, rs); 
 	     gastResource.getContents().add(root);
 	     saveResource(gastResource);
 	}
-		/*
-			JaMoPPParserAPI parser = new JaMoPPJDTParser();
-			JavaRoot root = ContainersFactory.eINSTANCE.createEmptyModel();
-			ResourceSet set = parser.parseUri(URI.createURI(INPUT));
-			Resource resource = createAndAddResource(OUTPUT, new String[] {"xml","java" }, set);
-			
-			resource.getContents().add(root);
-		    saveResource(resource);
-		  }
 		
-		*/
+	
 	  public static Resource createAndAddResource(String outputFile, String[] fileextensions, ResourceSet rs) {
 		     for (String fileext : fileextensions) {
 		        rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put(fileext, new XMLResourceFactoryImpl());
@@ -83,7 +175,7 @@ public class JaMoPPStandalone {
 		  }
 }
 	
-	
+
 	
 	     
 	        
