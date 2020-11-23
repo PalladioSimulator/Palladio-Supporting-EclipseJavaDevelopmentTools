e4777e2cdfdf85e0976bfef5b586f44b7bdc7ac3
Martin Armbruster <armbrustermartin@googlemail.com>
2020-11-24 00:00:56 +0100

    Added missing factories for URI fragments.

--- bundles/jamopp.resource.resolve/src/org/emftext/language/java/resource/java/JavaContextDependentURIFragmentFactoryFactory.java
+++ bundles/jamopp.resource.resolve/src/org/emftext/language/java/resource/java/JavaContextDependentURIFragmentFactoryFactory.java
@@ -3,10 +3,20 @@ package org.emftext.language.java.resource.java;
 import org.emftext.language.java.annotations.AnnotationAttributeSetting;
 import org.emftext.language.java.annotations.AnnotationInstance;
 import org.emftext.language.java.classifiers.Classifier;
+import org.emftext.language.java.classifiers.ConcreteClassifier;
+import org.emftext.language.java.imports.ClassifierImport;
+import org.emftext.language.java.imports.StaticMemberImport;
 import org.emftext.language.java.members.InterfaceMethod;
+import org.emftext.language.java.references.ElementReference;
+import org.emftext.language.java.references.ReferenceableElement;
 import org.emftext.language.java.resource.java.analysis.AnnotationAttributeSettingAttributeReferenceResolver;
 import org.emftext.language.java.resource.java.analysis.AnnotationInstanceAnnotationReferenceResolver;
+import org.emftext.language.java.resource.java.analysis.ClassifierImportClassifierReferenceResolver;
+import org.emftext.language.java.resource.java.analysis.ClassifierReferenceTargetReferenceResolver;
+import org.emftext.language.java.resource.java.analysis.ElementReferenceTargetReferenceResolver;
+import org.emftext.language.java.resource.java.analysis.StaticMemberImportStaticMembersReferenceResolver;
 import org.emftext.language.java.resource.java.mopp.JavaContextDependentURIFragmentFactory;
+import org.emftext.language.java.types.ClassifierReference;
 
 public final class JavaContextDependentURIFragmentFactoryFactory {
 	private JavaContextDependentURIFragmentFactoryFactory() {
@@ -17,4 +27,16 @@ public final class JavaContextDependentURIFragmentFactoryFactory {
 	
 	public static final IJavaContextDependentURIFragmentFactory<AnnotationAttributeSetting, InterfaceMethod> ANNOTATION_ATTRIBUTE_SETTING_INTERFACE_METHOD_FACTORY
 		= new JavaContextDependentURIFragmentFactory<>(new AnnotationAttributeSettingAttributeReferenceResolver());
+	
+	public static final IJavaContextDependentURIFragmentFactory<ClassifierImport, ConcreteClassifier> CLASSIFIER_IMPORT_CLASSIFIER_REFERENCE_FACTORY
+		= new JavaContextDependentURIFragmentFactory<>(new ClassifierImportClassifierReferenceResolver());
+	
+	public static final IJavaContextDependentURIFragmentFactory<ClassifierReference, Classifier> CLASSIFIER_REFERENCE_TARGET_REFERENCE_FACTORY
+		= new JavaContextDependentURIFragmentFactory<>(new ClassifierReferenceTargetReferenceResolver());
+	
+	public static final IJavaContextDependentURIFragmentFactory<ElementReference, ReferenceableElement> ELEMENT_REFERENCE_TARGET_REFERENCE_FACTORY
+		= new JavaContextDependentURIFragmentFactory<>(new ElementReferenceTargetReferenceResolver());
+	
+	public static final IJavaContextDependentURIFragmentFactory<StaticMemberImport, ReferenceableElement> STATIC_MEMER_IMPORT_STATIC_MEMBERS_REFERENCE_FACTORY
+		= new JavaContextDependentURIFragmentFactory<>(new StaticMemberImportStaticMembersReferenceResolver());
 }
