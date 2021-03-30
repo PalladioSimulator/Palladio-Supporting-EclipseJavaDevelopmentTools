487f275f11e8050c91db0d2530a00a9cbcacdcc2
Martin Armbruster <armbrustermartin@googlemail.com>
2021-03-30 16:03:04 +0200

    Extended the ClassFileParserTest case for static calls.

--- tests/org.emftext.language.java.test/src-input/StaticCalls.java
+++ tests/org.emftext.language.java.test/src-input/StaticCalls.java
@@ -1,9 +1,15 @@
 import static staticcalltarget.CallTargetsProvider.anotherStaticMethod;
+import static staticcalltarget.CallTargetsProvider.staticWithParameter;
 
 public class StaticCalls {
 	public void m() {
 		String s = AClass.createA("empty").getName();
 		System.out.println(s);
 		System.out.println(anotherStaticMethod());
+		staticWithParameter(1);
+		staticWithParameter(0.0);
+		staticWithParameter(0, 0);
+		staticWithParameter(0, () -> s.chars());
+		staticWithParameter(0, (i) -> i.byteValue());
 	}
 }
--- tests/org.emftext.language.java.test/src-input/staticcalltarget/CallTargetsProvider.java
+++ tests/org.emftext.language.java.test/src-input/staticcalltarget/CallTargetsProvider.java
@@ -1,7 +1,26 @@
 package staticcalltarget;
 
+import java.util.function.Function;
+
 public class CallTargetsProvider {
 	public static String anotherStaticMethod() {
 		return "";
+	}
+	
+	public static void staticWithParameter(int i) {
+	}
+	
+	public static void staticWithParameter(double d) {
+	}
+	
+	public static void staticWithParameter(int i, int j) {
+	}
+	
+	public static void staticWithParameter(int i, Runnable r) {
+		r.run();
+	}
+	
+	public static <T,R> void staticWithParameter(T i, Function<T, R> s) {
+		s.apply(i);
 	}
 }
