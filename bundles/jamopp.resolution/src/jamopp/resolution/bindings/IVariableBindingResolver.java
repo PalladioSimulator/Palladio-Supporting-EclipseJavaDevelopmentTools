2d680252529e9b6a4db88038cd6bae1842ac3c81
Martin Armbruster <armbrustermartin@googlemail.com>
2021-04-04 21:23:20 +0200

    Implemented a part of the IVariableBindingResolver.

--- bundles/jamopp.resolution/src/jamopp/resolution/bindings/IVariableBindingResolver.java
+++ bundles/jamopp.resolution/src/jamopp/resolution/bindings/IVariableBindingResolver.java
@@ -2,6 +2,11 @@ package jamopp.resolution.bindings;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.emftext.language.java.classifiers.ConcreteClassifier;
+import org.emftext.language.java.classifiers.Enumeration;
+import org.emftext.language.java.members.EnumConstant;
+import org.emftext.language.java.members.Field;
+import org.emftext.language.java.members.Member;
 
 class IVariableBindingResolver extends AbstractBindingResolver<IVariableBinding> {
 	protected IVariableBindingResolver(CentralBindingBasedResolver parentResolver) {
@@ -10,6 +15,22 @@ class IVariableBindingResolver extends AbstractBindingResolver<IVariableBinding>
 
 	@Override
 	protected EObject resolve(IVariableBinding binding) {
+		if (binding.isEnumConstant()) {
+			Enumeration e = (Enumeration) this.getParentResolver().resolve(binding.getDeclaringClass());
+			for (EnumConstant enCons : e.getConstants()) {
+				if (enCons.getName().equals(binding.getName())) {
+					return enCons;
+				}
+			}
+		} else if (binding.isField()) {
+			ConcreteClassifier container = (ConcreteClassifier) this.getParentResolver()
+					.resolve(binding.getDeclaringClass());
+			for (Member mem : container.getMembers()) {
+				if (mem instanceof Field && ((Field) mem).getName().equals(binding.getName())) {
+					return mem;
+				}
+			}
+		}
 		return null;
 	}
 }
