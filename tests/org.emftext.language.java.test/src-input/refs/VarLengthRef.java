815f20580c246e376af5a83467690372adfb21e6
Martin Armbruster <armbrustermartin@googlemail.com>
2021-04-08 17:21:06 +0200

    Extended the test case for the usage of methods with variable length parameters.

--- tests/org.emftext.language.java.test/src-input/refs/VarLengthRef.java
+++ tests/org.emftext.language.java.test/src-input/refs/VarLengthRef.java
@@ -1,10 +1,19 @@
 package refs;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
 
 public class VarLengthRef {
 	public void m() {
 		List<String> list = Arrays.asList("A", "B", "C");
+		try {
+			Files.write(Paths.get(""), new byte[] {}, StandardOpenOption.CREATE,
+					StandardOpenOption.WRITE);
+		} catch (IOException e) {
+		}
 	}
 }
