aa0fcc6d9d1b26e067267fed86ed017f35c38103
Martin Armbruster <armbrustermartin@googlemail.com>
2022-04-04 23:53:03 +0200

    Added a test case for the TrivialRecovery and TEAMMATES.

---
+++ tests/org.emftext.language.java.test/src/jamopp/test/recovery/package-info.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2022, Martin Armbruster
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Martin Armbruster
+ *      - Initial implementation
+ ******************************************************************************/
+
+/**
+ * Contains  tests for recovery strategies.
+ */
+package jamopp.test.recovery;
